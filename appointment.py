19c7cd34db85e3832e9d320f94bd95dc5e068028
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2021-04-29 05:35:50 +0200

    Update appointment.py
    1. Replace names 
    2. change colors 
    3. deleted (phone)
    4. commented "import tkinter.messin_carebox"
    5. I am trying to make an image background

--- appointment.py
+++ appointment.py
@@ -1,7 +1,5 @@
-# import modules
-from tkinter import *
 import sqlite3
-import tkinter.messagebox
+#import tkinter.messin_carebox
 # connect to the databse.
 conn = sqlite3.connect('database.db')
 # cursor to move around the databse
@@ -16,60 +14,64 @@ class Application:
         self.master = master
 
         # creating the frames in the master
-        self.left = Frame(master, width=800, height=720, bg='lightgreen')
+        self.left = Frame(master, width=800, height=720, bg='cyan')
+        self.bg = PhotoImage(file = 'bg.jpg')
+        self.label = Label(self.left, image = self.bg)
+        self.label.place(x=0, y=0, relwidth = 1, relheight = 1)
         self.left.pack(side=LEFT)
 
         self.right = Frame(master, width=400, height=720, bg='steelblue')
         self.right.pack(side=RIGHT)
-
+        
+           
         # labels for the window
-        self.heading = Label(self.left, text="ABC Hospital Appointments", font=('arial 40 bold'), fg='black', bg='lightgreen')
+        self.heading = Label(self.left, text="Hospital Admins", font=('arial 40 bold'), fg='black', bg='cyan')
         self.heading.place(x=0, y=0)
-        # patients name
-        self.name = Label(self.left, text="Patient's Name", font=('arial 18 bold'), fg='black', bg='lightgreen')
-        self.name.place(x=0, y=100)
+        # patients ID
+        self.ID = Label(self.left, text="Hospital ID", font=('arial 18 bold'), fg='black', bg='cyan')
+        self.ID.place(x=0, y=100)
 
-        # age
-        self.age = Label(self.left, text="Age", font=('arial 18 bold'), fg='black', bg='lightgreen')
-        self.age.place(x=0, y=140)
+        # in_care
+        self.in_care = Label(self.left, text="Bed in intensive care", font=('arial 18 bold'), fg='black', bg='cyan')
+        self.in_care.place(x=0, y=140)
 
-        # gender
-        self.gender = Label(self.left, text="Gender", font=('arial 18 bold'), fg='black', bg='lightgreen')
-        self.gender.place(x=0, y=180)
+        # ven
+        self.ven = Label(self.left, text="Used Ventilators", font=('arial 18 bold'), fg='black', bg='cyan')
+        self.ven.place(x=0, y=180)
 
-        # location
-        self.location = Label(self.left, text="Location", font=('arial 18 bold'), fg='black', bg='lightgreen')
-        self.location.place(x=0, y=220)
+        # max_bed
+        self.max_bed = Label(self.left, text="Max bed", font=('arial 18 bold'), fg='black', bg='cyan')
+        self.max_bed.place(x=0, y=220)
 
-        # appointment time
-        self.time = Label(self.left, text="Appointment Time", font=('arial 18 bold'), fg='black', bg='lightgreen')
-        self.time.place(x=0, y=260)
+        # appointment max_ven
+        self.max_ven = Label(self.left, text="Max ven", font=('arial 18 bold'), fg='black', bg='cyan')
+        self.max_ven.place(x=0, y=260)
 
-        # phone
-        self.phone = Label(self.left, text="Phone Number", font=('arial 18 bold'), fg='black', bg='lightgreen')
-        self.phone.place(x=0, y=300)
+        # # phone
+        # self.phone = Label(self.left, text="Phone Number", font=('arial 18 bold'), fg='black', bg='cyan')
+        # self.phone.place(x=0, y=300)
 
         # Entries for all labels============================================================
-        self.name_ent = Entry(self.left, width=30)
-        self.name_ent.place(x=250, y=100)
+        self.ID_ent = Entry(self.left, width=30)
+        self.ID_ent.place(x=280, y=100)
 
-        self.age_ent = Entry(self.left, width=30)
-        self.age_ent.place(x=250, y=140)
+        self.in_care_ent = Entry(self.left, width=30)
+        self.in_care_ent.place(x=280, y=140)
     
-        self.gender_ent = Entry(self.left, width=30)
-        self.gender_ent.place(x=250, y=180)
+        self.ven_ent = Entry(self.left, width=30)
+        self.ven_ent.place(x=280, y=180)
 
-        self.location_ent = Entry(self.left, width=30)
-        self.location_ent.place(x=250, y=220)
+        self.max_bed_ent = Entry(self.left, width=30)
+        self.max_bed_ent.place(x=280, y=220)
 
-        self.time_ent = Entry(self.left, width=30)
-        self.time_ent.place(x=250, y=260)
+        self.max_ven_ent = Entry(self.left, width=30)
+        self.max_ven_ent.place(x=280, y=260)
 
         self.phone_ent = Entry(self.left, width=30)
-        self.phone_ent.place(x=250, y=300)
+        self.phone_ent.place(x=280, y=300)
 
         # button to perform a command
-        self.submit = Button(self.left, text="Add Appointment", width=20, height=2, bg='steelblue', command=self.add_appointment)
+        self.submit = Button(self.left, text="Add", width=20, height=4, bg='steelblue', command=self.add_appointment)
         self.submit.place(x=300, y=340)
     
         # getting the number of appointments fixed to view in the log
@@ -92,22 +94,22 @@ class Application:
     # funtion to call when the submit button is clicked
     def add_appointment(self):
         # getting the user inputs
-        self.val1 = self.name_ent.get()
-        self.val2 = self.age_ent.get()
-        self.val3 = self.gender_ent.get()
-        self.val4 = self.location_ent.get()
-        self.val5 = self.time_ent.get()
+        self.val1 = self.ID_ent.get()
+        self.val2 = self.in_care_ent.get()
+        self.val3 = self.ven_ent.get()
+        self.val4 = self.max_bed_ent.get()
+        self.val5 = self.max_ven_ent.get()
         self.val6 = self.phone_ent.get()
 
         # checking if the user input is empty
         if self.val1 == '' or self.val2 == '' or self.val3 == '' or self.val4 == '' or self.val5 == '':
-            tkinter.messagebox.showinfo("Warning", "Please Fill Up All Boxes")
+            tkinter.messin_carebox.showinfo("Warning", "Please Fill Up All Boxes")
         else:
             # now we add to the database
-            sql = "INSERT INTO 'appointments' (name, age, gender, location, scheduled_time, phone) VALUES(?, ?, ?, ?, ?, ?)"
+            sql = "INSERT INTO 'appointments' (ID, in_care, ven, max_bed, scheduled_max_ven, phone) VALUES(?, ?, ?, ?, ?, ?)"
             c.execute(sql, (self.val1, self.val2, self.val3, self.val4, self.val5, self.val6))
             conn.commit()
-            tkinter.messagebox.showinfo("Success", "Appointment for " +str(self.val1) + " has been created" )
+            tkinter.messin_carebox.showinfo("Success", "Appointment for " +str(self.val1) + " has been created" )
             
 
             self.box.insert(END, 'Appointment fixed for ' + str(self.val1) + ' at ' + str(self.val5))
